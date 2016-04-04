0ec154f47418877b1d5dcdcf37ed1125f11bb39a
jérémy <wasner83@gmail.com>
2016-04-04 17:21:50 +0200

    /*non fonctionnel */ Problème avec la base de donnée : NullPointerException

--- app/src/main/java/com/example/mael/androidproject2/MainActivity.java
+++ app/src/main/java/com/example/mael/androidproject2/MainActivity.java
@@ -8,13 +8,23 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.mael.androidproject2.formulaire.BaseDonne.AddBaseJSON;
 import com.example.mael.androidproject2.formulaire.FormFragment;
+import com.example.mael.androidproject2.formulaire.JSONGetter;
 import com.example.mael.androidproject2.liste.BaseDonne.AddBase;
 import com.example.mael.androidproject2.liste.ItemListeFrigo;
 import com.example.mael.androidproject2.map.MapsActivity;
 import com.example.mael.androidproject2.reseau.ItemReseau;
 import com.google.android.gms.maps.MapsInitializer;
 
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import traitement.Produit;
 
@@ -26,6 +36,13 @@ public class MainActivity extends AppCompatActivity implements OnFragmentInterac
     public static Context CONTEXT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Partie initialisation BD avec le json des aliments
+        addBDJsonAliment();
+        //AddBaseJSON BDJSON = AddBaseJSON.getInstance(CONTEXT);
+        //List<String> aliment = BDJSON.getProduit();
+        /*for (String s: aliment) {
+            System.out.println(s);
+        }*/
         super.onCreate(savedInstanceState);
         CONTEXT = getBaseContext();
         fragmentManager = getSupportFragmentManager();
@@ -56,5 +73,26 @@ public class MainActivity extends AppCompatActivity implements OnFragmentInterac
         @Override
         public CharSequence getPageTitle(int position){
             return "OBJECT " + (position + 1);}
+    }
+
+    public void addBDJsonAliment() {
+        AddBaseJSON ad = AddBaseJSON.getInstance(MainActivity.CONTEXT);
+        JSONObject jsonObject = null;
+        try {
+            jsonObject = new JSONGetter("http://phpiutaix2.alwaysdata.net/foods-fr.json").execute().get();
+        } catch (MalformedURLException | ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        if (jsonObject != null)
+            try {
+                JSONArray jArray = jsonObject.getJSONObject("foods").getJSONArray("fgid");
+                for (int i = 0; i < jArray.length(); ++i) {
+                    JSONObject object = jArray.getJSONObject(i);
+                    ad.addProduit(object.getString("food"));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
     }
 }
