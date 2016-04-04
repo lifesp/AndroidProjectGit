b48732dc85dfd6f8a7d843e8f7d7bc312cf9b86a
jérémy <wasner83@gmail.com>
2016-04-04 15:05:12 +0200

    Résolution problème R dans maps_activity

--- app/src/main/java/com/example/mael/androidproject2/map/MapsActivity.java
+++ app/src/main/java/com/example/mael/androidproject2/map/MapsActivity.java
@@ -3,6 +3,7 @@ package com.example.mael.androidproject2.map;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.example.mael.androidproject2.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
